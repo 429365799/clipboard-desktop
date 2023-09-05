acbe262e071d4f843a099c59466e2c064537198a
小强 <429365799@qq.com>
2023-09-05 23:19:56 +0800

    feat: refactor project struct

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,126 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::fs;
-use std::process::Command;
-
-use clipboardrs::api::{read_clipboard_data, write_clipboard_data, ClipboardData, ClipboardFile};
-use serde::{ser::SerializeStruct, Serialize};
 use tauri::{
-    api::path::cache_dir, CustomMenuItem, Manager, Pixel, Position, Size, SystemTray,
+    CustomMenuItem, Manager, Pixel, Position, Size, SystemTray,
     SystemTrayMenu
 };
-use uuid::Uuid;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
-struct MyFile(ClipboardFile);
-
-impl Serialize for MyFile {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        let mut s = serializer.serialize_struct("MyFile", 2)?;
-        s.serialize_field("path", &self.0.path)?;
-        s.serialize_field("size", &self.0.size)?;
-        s.end()
-    }
+mod commands {
+    pub(crate) mod get_clipboard_list;
+    pub(crate) mod show_in_folder;
 }
 
-struct MyClipboardData {
-    len: usize,
-    text: Option<String>,
-    html: Option<String>,
-    image: Option<String>,
-    files: Option<Vec<MyFile>>,
-}
-
-impl MyClipboardData {
-    fn new(raw_data: ClipboardData) -> MyClipboardData {
-        let mut len: usize = 0;
-        let mut data = MyClipboardData {
-            len: 0,
-            text: None,
-            html: None,
-            image: None,
-            files: None,
-        };
-
-        if let Some(val) = raw_data.text {
-            len += 1;
-            data.text = Some(val);
-        }
-        if let Some(val) = raw_data.html {
-            len += 1;
-            data.html = Some(val);
-        }
-        if let Some(val) = raw_data.image {
-            if let Some(p) = cache_dir() {
-                let parent_dir = p.join("clipboard-tauri");
-                let path = parent_dir.join(format!("{}.png", Uuid::new_v4()));
-                if let Ok(()) = fs::create_dir_all(parent_dir) {
-                    if let Some(path) = path.to_str() {
-                        if let Ok(()) = val.save(path) {
-                            len += 1;
-                            data.image = Some(path.to_string());
-                        }
-                    }
-                }
-            }
-        }
-        if let Some(val) = raw_data.files {
-            let mut files = vec![];
-            for f in val {
-                files.push(MyFile(f));
-            }
-            data.files = Some(files);
-        }
-
-        data.len = len;
-
-        data
-    }
-}
-
-impl Serialize for MyClipboardData {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        let mut s = serializer.serialize_struct("MyClipboardData", self.len)?;
-        if let Some(text) = &self.text {
-            s.serialize_field("text", text)?;
-        }
-        if let Some(html) = &self.html {
-            s.serialize_field("html", html)?;
-        }
-        if let Some(img) = &self.image {
-            s.serialize_field("image", img)?;
-        }
-        if let Some(files) = &self.files {
-            s.serialize_field("files", files)?;
-        }
-        s.end()
-    }
-}
-
-#[tauri::command]
-fn get_clipboard_list() -> Vec<MyClipboardData> {
-    let mut list = vec![];
-    if let Ok(data) = read_clipboard_data() {
-        list.push(MyClipboardData::new(data));
-    }
-
-    list
-}
-
-#[tauri::command]
-fn show_in_folder(path: &str) {
-    Command::new("explorer")
-        .args(["/select,", &path]) // The comma after select is not a typo
-        .spawn()
-        .unwrap();
+mod structs {
+    pub(crate) mod clipboard;
 }
 
 fn main() {
@@ -160,13 +54,12 @@ fn main() {
                 })
                 .build(app)?;
 
-            app.global_shortcut_manager().register("Ctrl+Shift+V", || {
-                println!("！！！");
-            }).expect("快捷键注册失败");
-
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![show_in_folder, get_clipboard_list])
+        .invoke_handler(tauri::generate_handler![
+            crate::commands::get_clipboard_list::get_clipboard_list,
+            crate::commands::show_in_folder::show_in_folder,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
